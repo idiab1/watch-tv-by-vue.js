7cd8c932627e8f1bdbb74ffa07f86fa42a88706e
islamdiab-stack <islamdiab967@gmail.com>
2021-05-16 12:53:12 +0200

    Style footer

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -12,6 +12,24 @@
 
 <style lang="scss">
 .footer {
-  padding: 30px 0;
+  padding: 20px;
+  background-color: #222831;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .footer-content{
+    p{
+      color: #fff;
+      font-size: 17px;
+      letter-spacing: 1px;
+      text-transform: capitalize;
+      font-weight: 300;
+      a {
+        font-weight: bold;
+        color: #42b983;
+        
+      }
+    }
+  }
 }
 </style>
