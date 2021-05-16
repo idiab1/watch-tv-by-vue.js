cb3275b8d014f3b0a3e41eeb574757a85a0a407a
islamdiab-stack <islamdiab967@gmail.com>
2021-05-16 10:11:26 +0200

    Create new component for navbar and style this component

---
+++ src/components/app-navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-logo">
+          <router-link to="/">Home</router-link>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+
+<style lang="scss">
+.navbar{
+  padding: 20px;
+  background-color: #222831;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
