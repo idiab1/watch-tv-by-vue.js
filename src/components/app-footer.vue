d6796a3354dc83ad17f56485854d0f97aab25700
islamdiab-stack <islamdiab967@gmail.com>
2021-05-16 10:06:13 +0200

    Create footer component

---
+++ src/components/app-footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <footer class="footer">
+      <div class="container">
+        <div class="footer-content">
+          <p>&copy; Copyright all reserved 2021 coding by <a href="#">Islam Diab</a></p>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss">
+.footer {
+  padding: 30px 0;
+}
+</style>
