8d07008b845bc3a5edd551a64ab6798f6c54355a
islamdiab-stack <islamdiab967@gmail.com>
2021-05-17 09:04:41 +0200

    Create new route for movie details

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import MovieDetails from "../views/MovieDetails.vue";
 
 const routes = [
   {
@@ -7,6 +8,11 @@ const routes = [
     name: "Home",
     component: Home,
   },
+  {
+    path: '/movie/:id',
+    name: "MovieDetails",
+    component: MovieDetails,
+  }
 ];
 
 const router = createRouter({
